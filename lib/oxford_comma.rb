df4f4eb2fbd4bab6ca1f290891b6df141d8eeaeb
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-16 02:28:37 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
-
+  case array.length  
+    when 1 
+      return array.join
+    when 2 
+      return array.join(" and ")
+    when 3
+      final = array.pop
+      string = ""
+      
+      array.each  {|element| string << element + ", "}
+      
+      string << "and " + final 
+    else
+      final = array.pop
+      string = ""
+      
+      array.each  {|element| string << element + ", "}
+      
+      string << "and " + final 
+    end  
 end
