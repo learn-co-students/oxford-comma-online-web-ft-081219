1e93b7b7619df5091fb0f5e3118afd155ee453fc
Emilia ☆ <emilia.mena26@gmail.com>
2019-08-17 15:27:37 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length == 2 
+    return array.join(" and ")
+  elsif array.length >= 3
+  last_item = "and #{array[-1]}"
+    array.pop
+    array << last_item
+    return array.join(", ")
+  else
+    return array.join()
+  end
 end
+
+
+  
